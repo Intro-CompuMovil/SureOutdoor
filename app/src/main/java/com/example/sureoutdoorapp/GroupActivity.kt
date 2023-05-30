97c8d7ae1e0da754519fdb9cd427b147b86163aa
Frank5005 <frankill.5005@gmail.com>
2023-05-30 02:54:30 -0500

    Grupal?: Check Queda faltando mostrarlos

--- app/src/main/java/com/example/sureoutdoorapp/GroupActivity.kt
+++ app/src/main/java/com/example/sureoutdoorapp/GroupActivity.kt
@@ -6,19 +6,32 @@ import android.widget.Button
 import android.widget.ImageButton
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.sureoutdoorapp.databinding.GroupBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 //import androidx.appcompat.widget.Toolbar
 
 class GroupActivity : AppCompatActivity() {
+
+    lateinit var database: FirebaseFirestore
+
+    private lateinit var binding: GroupBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.group)
+        //setContentView(R.layout.group)
+        binding = GroupBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
         supportActionBar?.hide()
 
         //Recibe el correo del usuario actual
         var email = intent.getStringExtra("email").toString()
+
+        //Inicializa la base
+        database = FirebaseFirestore.getInstance()
 
         //Botón para cerrar sesión
 
@@ -93,7 +106,27 @@ class GroupActivity : AppCompatActivity() {
         val sendAllButton = findViewById<Button>(R.id.sendAllButton)
 
         sendAllButton.setOnClickListener{
-            Toast.makeText(applicationContext, "Convocatoria compartida a todos", Toast.LENGTH_LONG).show()
+
+            //Verificar que todos los campos estén llenos
+            if(binding.planName.text.isNotEmpty() && binding.placeName.text.isNotEmpty() && binding.dateEle.text.isNotEmpty() &&
+                    binding.timeEle.text.isNotEmpty()){
+                //Se puede proceder a guardar
+                val data = hashMapOf("name" to binding.planName.text.toString(),
+                    "place" to binding.placeName.text.toString(),
+                    "date" to binding.dateEle.text.toString(),
+                    "time" to binding.timeEle.text.toString(),
+                    "person" to email)
+                database.collection("plans").document(binding.planName.text.toString()).set(data).addOnSuccessListener {
+                    //Se pudo guardar satisfactoriamente
+                    Toast.makeText(applicationContext, "Convocatoria compartida a todos", Toast.LENGTH_LONG).show()
+                }
+                    .addOnFailureListener{
+                        Toast.makeText(applicationContext, "Error al crear el plan, intente de nuevo", Toast.LENGTH_LONG).show()
+                    }
+            }else{
+                Toast.makeText(applicationContext, "Llene todos los campos para poder crear el plan", Toast.LENGTH_LONG).show()
+            }
+
         }
 
     }
